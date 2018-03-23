472f95355a5467251beffc1012becab4bd694c6d
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-03-23 13:14:58 -0400

    Set up Index.js
    - Added all of the necessary imports
    - Created the redux store
    - Wrapped the entire app in a provider to have access to the store
    - Set up the routes

--- auth_jokes/src/index.js
+++ auth_jokes/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Jokes from './components/Jokes';
+import RequireAuth from './components/HOC/RequireAuth';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import rootReducer from './reducers/RootReducer';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(rootReducer)}>
+    <Router>
+      <div>
+        <Route path="/" component={App} />
+        <Route path="/login" component={SignIn} />
+        <Route path="/users" component={SignUp} />
+        <Route path="/jokes" component={RequireAuth(Jokes)} />
+      </div>
+    </Router>
+  </Provider>, 
+  document.getElementById('root'));
