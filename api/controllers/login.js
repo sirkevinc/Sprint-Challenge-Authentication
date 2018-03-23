22dd59c3c1ae611f2455d89dce55352950f15b51
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-03-23 15:32:40 -0400

    Moved toLowerCase into Server
    Moved toLowerCase from client into server

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -3,7 +3,8 @@ const { mysecret } = require('../../config');
 const User = require('../models/userModels');
 
 const login = (req, res) => {
-  const { username, password } = req.body;
+  let { username, password } = req.body;
+  username = username.toLowerCase();
   User.findOne({ username }, (err, user) => {
     if (err) {
       res.status(403).json({ error: 'Invalid Username/Password' });
