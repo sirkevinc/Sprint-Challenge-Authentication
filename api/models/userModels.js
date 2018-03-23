08bcdcb1c393fb992e9b2c998a8de73f69a62665
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-03-23 11:16:56 -0400

    Implemented User Schema
    - Created schema which requires username and passwords
    - Used bcrypt to set up a pre hook on the save function
    - Defined a checkPassword method using bcrypt

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -8,6 +8,16 @@ const UserSchema = Schema({
   // create your user schema here.
   // username: required, unique and lowercase
   // password: required
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
 });
 
 UserSchema.pre('save', function(next) {
@@ -15,6 +25,11 @@ UserSchema.pre('save', function(next) {
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // if there is an error here you'll need to handle it by calling next(err);
   // Once the password is encrypted, call next() so that your userController and create a user
+  bcrypt.hash(this.password, 11, (err, hash) => {
+    if (err) return next(err);
+    this.password = hash;
+    next();
+  })
 });
 
 UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
@@ -22,6 +37,10 @@ UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+  bcrypt.compare(plainTextPW, this.password, (err, isMatch) => {
+    if (err) return callBack(err);
+    callBack(null, isMatch);
+  })
 };
 
 // if you're really stuck with this at this point, you can reference this document.
